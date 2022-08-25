99ea8093d82f4226c9fdc57ec6dadf993ccd2042
unknown <you@example.com>
2022-08-25 13:06:47 +0600

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import { FormGroup, FormControl, Validators } from '@angular/forms'
+
+
 
 @Component({
   selector: 'app-root',
@@ -7,4 +10,19 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'coverLetter';
+  now = new Date();
+  tempNow = this.now.toDateString();
+  date = this.tempNow.substr(4);
+  form = new FormGroup({
+    cname: new FormControl('', Validators.required),
+    location: new FormControl('', Validators.required),
+    position: new FormControl('', Validators.required)
+  });
+
+  onSubmit() {
+    console.log(this.form.value);
+    console.log(this.date);
+  }
+
+  
 }
